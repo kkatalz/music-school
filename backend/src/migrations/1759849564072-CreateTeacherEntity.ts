db659b11ec21b41b98d1df9aaec65193ef33487a
kkatalz <zlata.kiev2004@gmail.com>
2025-10-07 18:20:24 +0300

    Create teacher table (entity + migration).

---
+++ backend/src/migrations/1759849564072-CreateTeacherEntity.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateTeacherEntity1759849564072 implements MigrationInterface {
+  name = 'CreateTeacherEntity1759849564072';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "teachers" ("id" SERIAL NOT NULL, "first_name" character varying NOT NULL, "last_name" character varying NOT NULL, "phone" integer NOT NULL, "education" character varying NOT NULL, "start_work_date" TIMESTAMP NOT NULL DEFAULT now(), "email" character varying NOT NULL, "password" character varying NOT NULL, "is_head_teacher" boolean NOT NULL DEFAULT false, CONSTRAINT "PK_a8d4f83be3abe4c687b0a0093c8" PRIMARY KEY ("id"))`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "teachers"`);
+  }
+}
---
+++ backend/src/teacher/teacher.entity.ts
@@ -0,0 +1,35 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'teachers' })
+export class TeacherEntity {
+  @PrimaryGeneratedColumn('increment')
+  id: string;
+
+  @Column({ name: 'first_name' })
+  firstName: string;
+
+  @Column({ name: 'last_name' })
+  lastName: string;
+
+  @Column()
+  phone: number;
+
+  @Column()
+  education?: string;
+
+  @Column({
+    name: 'start_work_date',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  startWorkDate: Date;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ name: 'is_head_teacher', default: false })
+  isHeadTeacher: boolean;
+}
