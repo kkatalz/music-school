f142517463adc34924a689e64951344ce729baf9
kkatalz <zlata.kiev2004@gmail.com>
2025-10-17 16:50:50 +0300

    Add firstName, education, startWorkDate to teacherResponseDto and generateUserResponse.

--- backend/src/teacher/dto/teacherResponse.dto.ts
+++ backend/src/teacher/dto/teacherResponse.dto.ts
@@ -2,8 +2,11 @@ import { Role } from 'src/auth/types/role.enum';
 
 export class TeacherResponseDto {
   id: number;
+  firstName: string;
   lastName: string;
   phone: string;
+  education?: string;
+  startWorkDate: Date;
   email: string;
   token: string;
   isHeadTeacher: boolean;
