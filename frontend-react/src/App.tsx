ffb597093977fda24c338923f40a287a4f6c3d6c
kkatalz <zlata.kiev2004@gmail.com>
2025-10-19 14:19:21 +0300

    Install react vite, tailwind, react query and react router.

---
+++ frontend-react/src/App.tsx
@@ -0,0 +1,10 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
+
+const App = () => {
+  return <QueryClientProvider client={queryClient}>
+    
+  </QueryClientProvider>;
+};
+
+export default App;
