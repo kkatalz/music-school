03a653bb8b36df681a50acbe65c9154799a2fe2f
kkatalz <zlata.kiev2004@gmail.com>
2025-10-19 14:39:55 +0300

    Configure react router.

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -1,10 +1,17 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router";
 const queryClient = new QueryClient();
 
 const App = () => {
-  return <QueryClientProvider client={queryClient}>
-    
-  </QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 };
 
 export default App;
