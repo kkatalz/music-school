3a8b192bc0bb6fb73d09b352bfcb24b1f135e180
kkatalz <zlata.kiev2004@gmail.com>
2025-10-24 13:52:50 +0300

    Add Grade, CreateGrade and UpdateGrade types.

--- frontend-react/src/grades/grades.types.ts
+++ frontend-react/src/grades/grades.types.ts
@@ -1,7 +1,18 @@
-import type { SubjectNameResponse } from "../subjects/types/subjects.types";
+import type { SubjectNameResponse } from '../subjects/types/subjects.types';
 
 export interface Grade {
   id: number;
   subject?: SubjectNameResponse;
   value?: number;
+}
+
+export interface CreateGrade {
+  studentId: number;
+  subjectId: number;
+  teacherId: number;
+  value?: number;
+}
+
+export interface UpdateGrade {
+  value: number;
 }
